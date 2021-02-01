bfb77dd383f79ec098c41479ce3dad40ff5c13de
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-02-01 23:17:58 +0900

    app-barのボタンを切り替え

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,21 @@
 <template>
   <v-app v-if="isAuth">
     <v-card class="mx-auto" min-width="370">
-      <v-app-bar color="#BDBDBD">
+      <v-app-bar v-if="$route.path == '/'" color="#BDBDBD">
         <nuxt-link tag="div" to="/result">
           <v-btn> RM </v-btn>
         </nuxt-link>
         <v-spacer></v-spacer>
         <nuxt-link tag="div" to="/memo">
-          <v-btn color="" icon>
-            <v-icon large>mdi-plus</v-icon>
+          <v-btn>
+            <v-icon>mdi-plus</v-icon>
+          </v-btn>
+        </nuxt-link>
+      </v-app-bar>
+      <v-app-bar v-else color="#BDBDBD">
+        <nuxt-link tag="div" to="/">
+          <v-btn>
+            <v-icon> mdi-arrow-left </v-icon>
           </v-btn>
         </nuxt-link>
       </v-app-bar>
