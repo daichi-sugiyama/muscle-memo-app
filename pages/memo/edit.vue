3380621c08528618b0eea35715be486b3aa92185
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-02-07 10:10:56 +0900

    Revert "firestoreが読み込まれてからformを表示するように実装"
    This reverts commit 6ad3c394790af17d32480ededd11dd66dd0607c9.

--- pages/memo/edit.vue
+++ pages/memo/edit.vue
@@ -1,53 +1,15 @@
 <template>
-  <Form
-    v-if="loaded"
-    :menuData="menuData"
-    :bodyTarget="bodyTarget"
-    :programIdArray="programIdArray"
-  />
+  <Form />
 </template>
 
 <script>
-import Form from "~/components/Form.vue";
+import Form from '~/components/Form.vue'
 export default {
   components: {
-    Form,
+    Form
   },
-  data() {
-    return {
-      loaded: false,
-    };
-  },
-  computed: {
-    menuData: {
-      get() {
-        return this.$store.state.memo.menuData;
-      },
-      set(val) {
-        return console.log(val);
-      },
-    },
-    bodyTarget: {
-      get() {
-        return this.$store.state.memo.bodyTarget;
-      },
-      set(val) {
-        return console.log(val);
-      },
-    },
-    programIdArray: {
-      get() {
-        return this.$store.state.memo.programIdArray;
-      },
-      set(val) {
-        return console.log(val);
-      },
-    },
-  },
-  mounted: async function () {
-    this.loaded = false;
-    await this.$store.dispatch("memo/setEditFrom", this.$route.query.memoId);
-    this.loaded = true;
+  created: function () {
+    this.$store.dispatch("memo/setEditFrom", this.$route.query.memoId);
   },
 };
 </script>
--- pages/memo/index.vue
+++ pages/memo/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Form v-if="loaded" :menuData="menuData" :bodyTarget="bodyTarget" />
+  <Form />
 </template>
 
 <script>
@@ -8,33 +8,8 @@ export default {
   components: {
     Form,
   },
-  data() {
-    return {
-      loaded: false,
-    };
-  },
-  computed: {
-    menuData: {
-      get() {
-        return this.$store.state.memo.menuData;
-      },
-      set(val) {
-        return console.log(val);
-      },
-    },
-    bodyTarget: {
-      get() {
-        return this.$store.state.memo.bodyTarget;
-      },
-      set(val) {
-        return console.log(val);
-      },
-    },
-  },
-  mounted: async function () {
-    this.loaded = false;
-    await this.$store.dispatch("memo/initMemo");
-    this.loaded = true;
+  created: function () {
+    this.$store.dispatch("memo/initMemo");
   },
 };
 </script>
