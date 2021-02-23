9a1d07a88783360994cecd089a8db2717e95e0ef
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-02-23 12:53:55 +0900

    アプリタイトルを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - muscle-memo',
-    title: 'muscle-memo',
+    titleTemplate: '筋トレメモアプリ',
+    title: '筋トレメモアプリ',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
