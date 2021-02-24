f8296667d6f7cb7d84a7430121b7fa538732d4f4
daichi.sugiyama <d.sugiyama421@gmail.com>
2021-02-24 21:10:39 +0900

    ログイン時に説明文を追加

--- components/Login.vue
+++ components/Login.vue
@@ -1,5 +1,11 @@
 <template>
   <v-card class="mx-auto text-center">
+    <v-card-subtitle>
+      筋トレメモアプリです。<br>
+      動作お試しの方は「テストユーザでログイン」をクリックください。<br>
+      ※スマホ向けのため、スマホで閲覧することをおすすめします。<br>
+      <p class="font-weight-bold">開発概要は<a href="https://github.com/daichi-sugiyama/muscle-memo-app">こちら</a></p>
+    </v-card-subtitle>
     <div>
       <v-btn class="my-4" @click="signInGoogle">自分のGoogleアカウントでログイン</v-btn>
     </div>
